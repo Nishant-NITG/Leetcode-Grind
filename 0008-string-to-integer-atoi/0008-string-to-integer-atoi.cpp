797ecd834f593e0cc3e4257f9c4f1fc8b745d3d2
Nishant Sharma <nishantsharmanit2023@gmail.com>
2025-12-11 00:36:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (80.72%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i=0;
-        int n=s.size();
-        int sign=1;
-        long result=0;
+        int n = s.size();
+        long ans = 0;               // long to detect overflow
+        int i = 0;
+        int sign = 1;
 
-        while(i<n && s[i]==' ') i++;
+        // 1. Skip leading spaces
+        while (i < n && s[i] == ' ') i++;
 
-        if(i<n && (s[i]=='+' || s[i]=='-'))
-        {
-            sign=(s[i]=='-')?-1:1;
+        // 2. Check sign
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
             i++;
         }
 
-        while(i<n && isdigit(s[i]))
-        {
-            result=result*10+(s[i]-'0');
+        // 3. Convert digits
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
 
-            if (result * sign > INT_MAX) return INT_MAX;
-            if (result * sign < INT_MIN) return INT_MIN;
+            // 4. Overflow check BEFORE adding digit
+            if (ans > (LONG_MAX / 10) || 
+               (ans == LONG_MAX / 10 && digit > 7)) 
+            {
+                if (sign == 1) return INT_MAX;
+                else return INT_MIN;
+            }
 
+            ans = ans * 10 + digit;
             i++;
         }
-        return (int)(result * sign);
+
+        ans = ans * sign;
+
+        // 5. Clamp to 32-bit range
+        if (ans > INT_MAX) return INT_MAX;
+        if (ans < INT_MIN) return INT_MIN;
+
+        return (int)ans;
     }
 };
