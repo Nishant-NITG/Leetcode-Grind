8d86ca69b5c48cff2bfca307352412a9a447e74f
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-03-10 22:39:47 +0530

    Time: 0 ms (100%), Space: 16.5 MB (32.19%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,43 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-         if (!head || !head->next || k == 0) return head;
+        if(!head || !head->next|| k==0)
 
-        // Compute the length of the linked list
-        int length = 1;  // Start from 1 since we start at head
-        ListNode *tail = head;
+        {
+            return head;
+        }
+    // findingt length
+        ListNode* tail=head;
+        int length=1;
 
-        while (tail->next != NULL) {  // Find the last node
-            tail = tail->next;
+        while(tail->next)
+        {
+            tail=tail->next;
             length++;
         }
 
-        // Adjust k to avoid unnecessary full rotations
-        k = k % length;
-        if (k == 0) return head;
-
-        // Connect last node to head to form a circular list
-        tail->next = head;
-        ListNode *newtail = head;
-        for (int i = 0; i < length - k - 1; i++) {
-            newtail = newtail->next;
+        //reduce k
+        k=k%length;
+        if(k==0)
+        {
+            return head;
         }
 
-        // Break the cycle and set new head
-        ListNode *newhead = newtail->next;
-        newtail->next = NULL;
+        //find new tail
+        int steps=length-k-1;
+        ListNode* newTail=head;
 
-        return newhead;
+        for(int i=0;i<steps;i++)
+        {
+            newTail=newTail->next;
+        }
+
+        ListNode* newHead=newTail->next;
+
+        //rotate
+        newTail->next=NULL;
+        tail->next=head;
+
+        return newHead;
+
 
     }
 };
