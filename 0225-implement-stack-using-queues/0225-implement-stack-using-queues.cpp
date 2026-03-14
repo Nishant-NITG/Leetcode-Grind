dcf726cbfd0096ccc2143aa164424a1c35d18f6b
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-03-14 16:45:34 +0530

    Time: 0 ms (100%), Space: 9.5 MB (39.7%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,75 +1,40 @@
+#include <queue>
+using namespace std;
+
 class MyStack {
-public:
     queue<int> q1;
     queue<int> q2;
 
-    MyStack() {}
+public:
+    MyStack() {
+    }
 
-    // Push element onto stack
     void push(int x) {
-        if (q1.empty() && q2.empty())
-            q1.push(x);
-        else if (!q1.empty())
-            q1.push(x);
-        else
-            q2.push(x);
-    }
 
-    // Removes the element on top of the stack and returns it
-    int pop() {
-        if (empty()) return 0;
+        q2.push(x);
 
-        if (!q1.empty()) {
-            while (q1.size() > 1) {
-                q2.push(q1.front());
-                q1.pop();
-            }
-            int element = q1.front();
+        while(!q1.empty()){
+            q2.push(q1.front());
             q1.pop();
-            return element;
-        } else {
-            while (q2.size() > 1) {
-                q1.push(q2.front());
-                q2.pop();
-            }
-            int element = q2.front();
-            q2.pop();
-            return element;
         }
+
+        swap(q1, q2);
     }
 
-    // Get the top element
+    int pop() {
+
+        int val = q1.front();
+        q1.pop();
+        return val;
+    }
+
     int top() {
-        if (empty()) return 0;
 
-        int element;
-        if (!q1.empty()) {
-            while (!q1.empty()) {
-                element = q1.front();
-                q2.push(element);
-                q1.pop();
-            }
-        } else {
-            while (!q2.empty()) {
-                element = q2.front();
-                q1.push(element);
-                q2.pop();
-            }
-        }
-        return element;
+        return q1.front();
     }
 
-    // Returns whether the stack is empty
     bool empty() {
-        return q1.empty() && q2.empty();
+
+        return q1.empty();
     }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty(g);
- */
