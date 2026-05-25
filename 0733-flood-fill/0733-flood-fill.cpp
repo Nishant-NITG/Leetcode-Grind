a55f0c8026b3100a6235bb53a5c10cdde633e086
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-05-25 10:03:07 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int x, int y, int originalColor, int newColor) {
-        int m = image.size();
-        int n = image[0].size();
-        
-        // Boundary checks
-        if (x < 0 || x >= m || y < 0 || y >= n) return;
-        if (image[x][y] != originalColor) return;
-        
-        // Change the color
-        image[x][y] = newColor;
-        
-        // Visit 4-directionally connected pixels
-        dfs(image, x + 1, y, originalColor, newColor);
-        dfs(image, x - 1, y, originalColor, newColor);
-        dfs(image, x, y + 1, originalColor, newColor);
-        dfs(image, x, y - 1, originalColor, newColor);
+
+    void dfs(vector<vector<int>>& image,
+             int r,
+             int c,
+             int oldColor,
+             int newColor) {
+
+        int rows = image.size();
+        int cols = image[0].size();
+
+        // Boundary check
+        if(r < 0 || r >= rows || c < 0 || c >= cols) {
+            return;
+        }
+
+        // If color doesn't match original color
+        if(image[r][c] != oldColor) {
+            return;
+        }
+
+        // Change color
+        image[r][c] = newColor;
+
+        // Visit 4 directions
+
+        dfs(image, r-1, c, oldColor, newColor); // up
+        dfs(image, r+1, c, oldColor, newColor); // down
+        dfs(image, r, c-1, oldColor, newColor); // left
+        dfs(image, r, c+1, oldColor, newColor); // right
     }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-        
-        // Edge case: if the new color is same as original, no need to process
-        if (originalColor == color) return image;
-        
-        dfs(image, sr, sc, originalColor, color);
+    vector<vector<int>> floodFill(vector<vector<int>>& image,
+                                  int sr,
+                                  int sc,
+                                  int color) {
+
+        // Original color
+        int oldColor = image[sr][sc];
+
+        // If color already same
+        if(oldColor == color) {
+            return image;
+        }
+
+        // Start DFS
+        dfs(image, sr, sc, oldColor, color);
+
         return image;
     }
 };
