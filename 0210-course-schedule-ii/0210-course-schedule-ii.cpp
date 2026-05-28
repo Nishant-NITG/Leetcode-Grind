878243ab002f63df47d9116aafc620f0673be721
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-05-28 14:50:22 +0530

    Time: 3 ms (70.97%), Space: 18.3 MB (69.75%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,42 +1,60 @@
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-      
+
         vector<vector<int>> adj(numCourses);
+
         vector<int> indegree(numCourses, 0);
 
-        for (auto &pre : prerequisites) {
-            int u = pre[1];
-            int v = pre[0];
-            adj[u].push_back(v);
-            indegree[v]++;
+        // Build graph
+        for(auto &p : prerequisites)
+        {
+            int course = p[0];
+            int prereq = p[1];
+
+            adj[prereq].push_back(course);
+
+            indegree[course]++;
         }
 
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) q.push(i);
-        }
 
-       
-        vector<int> order;
-        while (!q.empty()) {
-            int course = q.front();
-            q.pop();
-            order.push_back(course);
-
-            for (int neigh : adj[course]) {
-                indegree[neigh]--;
-                if (indegree[neigh] == 0) q.push(neigh);
+        // Push all nodes with indegree 0
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i);
             }
         }
 
-     
-        if (order.size() != numCourses) return {};
-        return order;
+        vector<int> ans;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            // Reduce indegree of neighbors
+            for(int neighbor : adj[node])
+            {
+                indegree[neighbor]--;
+
+                if(indegree[neighbor] == 0)
+                {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        // Cycle exists
+        if(ans.size() != numCourses)
+        {
+            return {};
+        }
+
+        return ans;
     }
 };
