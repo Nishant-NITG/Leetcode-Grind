5a4db21d0667a9f2917a43207e7cb55831f9a92a
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-06-20 16:55:03 +0530

    Time: 0 ms (100%), Space: 31.2 MB (80.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start=0;
-        int end=nums.size()-1;
-        while(start<=end){
-            int mid=start + (end-start)/2;
-            if(nums[mid]==target){
-                cout<<target<<" exists in nums and its index is "<<mid<<endl;
-                return mid;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-                
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-                
-            }
-        }
-        return -1;
 
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while(l <= r)
+        {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            else if(nums[mid] < target)
+                l = mid + 1;
+
+            else
+                r = mid - 1;
+        }
+
+        return -1;
     }
 };
