d3ffc2bcb18864d2d2400236dc0f86dad9310572
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-06-22 17:16:45 +0530

    Time: 80 ms (16.92%), Space: 193.4 MB (20.27%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,44 +1,59 @@
 class Solution {
 public:
+
     int sumSubarrayMins(vector<int>& arr) {
-        const int mod=1e9+7;
-        int n=arr.size();
 
-        vector<int>pse(n),nse(n);
-        stack<int>st;
+        int n = arr.size();
 
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && arr[st.top()]>arr[i])
-            {
+        vector<int> prev(n);
+        vector<int> next(n);
+
+        stack<int> st;
+
+        // Previous Smaller
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.empty() &&
+                  arr[st.top()] > arr[i]) {
+
                 st.pop();
             }
 
-            pse[i]=st.empty()?-1:st.top();
+            prev[i] = st.empty() ? -1 : st.top();
+
             st.push(i);
         }
 
-        while(!st.empty()) st.pop();
+        while(!st.empty())
+            st.pop();
 
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && arr[st.top()]>=arr[i])
-            {
+        // Next Smaller
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            while(!st.empty() &&
+                  arr[st.top()] >= arr[i]) {
+
                 st.pop();
             }
 
+            next[i] = st.empty() ? n : st.top();
 
-            nse[i]=st.empty()?n:st.top();
             st.push(i);
         }
 
-        long long ans=0;
+        long long ans = 0;
+        long long MOD = 1e9 + 7;
 
-        for(int i=0;i<n;i++)
-        {
-            long long left=i-pse[i];
-            long long right=nse[i]-i;
-            ans=(ans+arr[i]*left%mod * right%mod)%mod;
+        for(int i = 0; i < n; i++) {
+
+            long long left = i - prev[i];
+            long long right = next[i] - i;
+
+            ans = (ans +
+                   (1LL * arr[i] * left * right) % MOD)
+                   % MOD;
         }
 
         return ans;
