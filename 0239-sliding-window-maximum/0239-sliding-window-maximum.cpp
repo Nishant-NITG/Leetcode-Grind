1a85b50140028c83e6d7f76a38dc708ff7377836
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-06-23 16:33:07 +0530

    Time: 15 ms (88.89%), Space: 139 MB (89.37%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;   // stores indices
-        vector<int> result;
 
-        for (int i = 0; i < nums.size(); i++) {
+        deque<int> dq;      // stores indices
+        vector<int> ans;
 
-            // Remove indices out of window
-            if (!dq.empty() && dq.front() == i - k) {
+        for(int i = 0; i < nums.size(); i++) {
+
+            // Remove indices that are no longer inside the window
+            while(!dq.empty() &&
+                  dq.front() <= i - k) {
+
                 dq.pop_front();
             }
 
-            // Remove smaller elements
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+            // Remove smaller elements from the back
+            // They can never become maximum again
+            while(!dq.empty() &&
+                  nums[dq.back()] < nums[i]) {
+
                 dq.pop_back();
             }
 
             // Add current index
             dq.push_back(i);
 
-            // Record answer
-            if (i >= k - 1) {
-                result.push_back(nums[dq.front()]);
+            // Window is complete
+            if(i >= k - 1) {
+
+                // Front always stores maximum element
+                ans.push_back(nums[dq.front()]);
             }
         }
 
-        return result;
+        return ans;
     }
 };
