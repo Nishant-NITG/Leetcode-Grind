8bc38ab4fa2c86b40ca41c950599f64b9470dd51
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-07-04 14:28:28 +0530

    Time: 0 ms (100%), Space: 37.2 MB (64.3%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,27 +1,45 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    void inorder(TreeNode* root, vector<int>& v) {
+    private:
+        void inorder(TreeNode* root, vector<int>& values) {
         if (!root) return;
-        inorder(root->left, v);
-        v.push_back(root->val);
-        inorder(root->right, v);
+        inorder(root->left, values);
+        values.push_back(root->val);
+        inorder(root->right, values);
     }
-
-
+public:
     bool findTarget(TreeNode* root, int k) {
-        vector<int>v;
-        inorder(root,v);
+        vector<int>values;
+        inorder(root,values);
 
-        int i=0,j=v.size()-1;
+        int left=0;
+        int right=values.size()-1;
 
-        while(i<j)
+        while(left<right)
         {
-            int sum=v[i]+v[j];
-
-            if(sum==k) return true;
-            else if(sum<k) i++;
-            else j--;
+            int sum=values[left]+values[right];
+            if(sum==k)
+            {
+                return true;
+            }
+            else if(sum<k)
+            {
+                left++;
+            }
+            else 
+            {
+                right--;
+            }
         }
         return false;
     }
