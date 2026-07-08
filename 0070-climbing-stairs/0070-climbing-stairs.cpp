669e0dd0f24f1beee8750a06164a7822160a2fbc
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-07-08 19:48:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // dp[i] = number of ways to reach step i
-        vector<int> dp(n + 1);
-        
-        dp[0] = 1;  // one way to be at ground
-        dp[1] = 1;  // one way to reach first step
-        
-        for (int i = 2; i <= n; i++) {
-            // To reach step i:
-            // 1. Take 1 step from step (i-1)
-            // 2. Take 2 steps from step (i-2)
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        
-        return dp[n];
+        vector<int>memo(n+1,-1);
+        return solve(n,memo);
+    }
+
+    int solve(int n,vector<int>& memo)
+    {
+        if(n<=1) return 1;
+
+        if(memo[n]!=-1) return memo[n];
+
+        return memo[n]=solve(n-1,memo)+solve(n-2,memo);
     }
 };
