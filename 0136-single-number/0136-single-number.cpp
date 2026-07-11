98c3a07b4f5f7b7022a0addc5c14c5a0bd82df1e
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-07-11 17:12:10 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.75%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result=0;
+
         for(int num:nums)
         {
-            result ^=num;
+            result^=num;
         }
         return result;
-        
     }
 };
