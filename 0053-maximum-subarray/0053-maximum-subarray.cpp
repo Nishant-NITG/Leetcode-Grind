fd30c18a1e39966ac46ce649a7c850eb8bac60e8
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-07-12 10:37:44 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int current=0;
+
         int maxSum=INT_MIN;
 
         for(int num:nums)
         {
             current=max(num,current+num);
+
             maxSum=max(maxSum,current);
         }
-
         return maxSum;
+        
     }
 };
