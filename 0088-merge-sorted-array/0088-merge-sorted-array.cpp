03c14931a3ad285803520a1e1c8e89a28de95f9c
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-07-12 11:39:35 +0530

    Time: 0 ms (100%), Space: 12.6 MB (9.54%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1; 
-        int j = n - 1; 
-        int k = m + n - 1; 
+        // Three pointers:
+        // p1: last element of nums1's actual data
+        // p2: last element of nums2
+        // p: last position in nums1 (where we'll place elements)
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
         
-  
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        // Merge from the back, comparing the largest remaining elements
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                // nums1's current element is larger, place it at the end
+                nums1[p] = nums1[p1];
+                p1--;
             } else {
-                nums1[k--] = nums2[j--];
+                // nums2's current element is larger or equal, place it
+                nums1[p] = nums2[p2];
+                p2--;
             }
+            p--;
         }
-
         
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        // If nums2 still has elements, they all go to the front
+        // nums1's remaining elements are already in the correct position
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
         }
     }
 };
