31429ec89ef24591e12698268856259da1540620
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-07-13 21:36:33 +0530

    Time: 2 ms (74.01%), Space: 38.4 MB (62.83%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-
+        // Count frequency of each task (A-Z)
         vector<int> freq(26, 0);
-
-       
-        for (char c : tasks) {
-            freq[c - 'A']++;
+        for (char task : tasks) {
+            freq[task - 'A']++;
         }
-
-       
-        int maxFreq = 0;
-        for (int f : freq) {
-            maxFreq = max(maxFreq, f);
-        }
-
         
+        // Find the maximum frequency and how many tasks have it
+        int maxFreq = 0;
         int maxCount = 0;
         for (int f : freq) {
-            if (f == maxFreq)
+            if (f > maxFreq) {
+                maxFreq = f;
+                maxCount = 1;
+            } else if (f == maxFreq) {
                 maxCount++;
+            }
         }
-
-   
-        int partCount = maxFreq - 1;
-        int partLength = n - (maxCount - 1);
-        int emptySlots = partCount * partLength;
-        int availableTasks = tasks.size() - maxFreq * maxCount;
-        int idles = max(0, emptySlots - availableTasks);
-
-        return tasks.size() + idles;
+        
+        // Formula: (maxFreq - 1) gaps of (n + 1) length
+        // plus maxCount tasks that have the same max frequency
+        int intervals = (maxFreq - 1) * (n + 1) + maxCount;
+        
+        // We need at least as many intervals as tasks
+        return max((int)tasks.size(), intervals);
     }
 };
