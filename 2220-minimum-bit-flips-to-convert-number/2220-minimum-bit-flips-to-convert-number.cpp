73f5ab8c14a77397ba8003a87c123d2179fcaadb
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-08-15 03:55:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.8%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int x=start^goal;
-        int count=0;
-
-        while(x)
-        {
-            x&=(x-1);
-            count++;
+        // XOR gives 1 where bits differ, 0 where they're same
+        int diff = start ^ goal;
+        
+        // Count the number of 1s in diff (set bits)
+        int flips = 0;
+        while (diff > 0) {
+            diff = diff & (diff - 1);  // Remove rightmost set bit
+            flips++;
         }
-        return count;
+        
+        return flips;
     }
 };
