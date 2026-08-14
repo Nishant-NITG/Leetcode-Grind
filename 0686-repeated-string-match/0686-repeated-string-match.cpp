c07a2740370c5fa7f8ea6f1ce0b282c93afe864b
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-08-15 01:45:43 +0530

    Time: 8 ms (25.08%), Space: 9.8 MB (46.09%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        string repeated = "";
-        int count = 0;
+        string repeated="";
+        int count=0;
 
-        // Repeat 'a' until length >= length of 'b'
-        while (repeated.length() < b.length()) {
-            repeated += a;
+        while(repeated.length()<b.length())
+        {
+            repeated+=a;
             count++;
         }
 
-        // Case 1: b fits inside current repeated string
-        if (repeated.find(b) != string::npos)
+        if(repeated.find(b)!=string::npos)
+        {
             return count;
+        }
 
-        // Case 2: Try one more repetition
-        repeated += a;
-        if (repeated.find(b) != string::npos)
-            return count + 1;
+        repeated+=a;
+        count++;
 
+        if(repeated.find(b)!=string::npos)
+        {
+            return count;
+        }
         return -1;
     }
 };
