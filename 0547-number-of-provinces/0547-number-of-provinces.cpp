33c5a73055f20e5dddd8ac0b4c110cc09e36b64e
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-08-18 17:13:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.65%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    // DFS helper that explores all cities in a province
-    void dfs(int city, vector<vector<int>>& isConnected, vector<bool>& visited) {
-        // Mark current city as part of this province
-        visited[city] = true;
-        int n = isConnected.size();
-        
-        // Instead of having an adjacency list, we scan the entire row
-        // This is O(n) per vertex, which is fine for n <= 200
-        for (int neighbor = 0; neighbor < n; neighbor++) {
-            // Check if there's a direct connection to this neighbor
-            // AND we haven't visited it yet
-            if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
-                // If connected, it belongs to the same province
-                dfs(neighbor, isConnected, visited);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+
+        vector<bool>visited(n,0);
+        int province=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {
+                province++;
+                dfs(i,isConnected,visited);
             }
         }
+        return province;
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<bool> visited(n, false);
-        int provinces = 0;
-        
-        // Iterate through all cities
-        for (int i = 0; i < n; i++) {
-            // If this city hasn't been visited, it starts a new province
-            if (!visited[i]) {
-                provinces++;
-                
-                // Explore the entire province from this city
-                dfs(i, isConnected, visited);
+    void dfs(int city,vector<vector<int>>& isConnected,vector<bool>&visited){
+        visited[city]=1;
+
+        for(int j=0;j<isConnected.size();j++)
+        {
+            if(isConnected[city][j]==1 && !visited[j])
+            {
+                dfs(j,isConnected,visited);
             }
         }
-        
-        return provinces;
     }
 };
