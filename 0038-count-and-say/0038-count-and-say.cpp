c8022f304f799369e9e088c6365eb1dd552b4f27
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-09-06 17:58:38 +0530

    Time: 7 ms (13.19%), Space: 9.9 MB (41.21%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1) return "1";
-
-        string current="1";
-        for(int i=2;i<=n;i++)
-        {
-            current=rle(current);
-        }
-        return current;
-    }
-
-
-    private:
-
-    string rle(const string& s)
-    {
-        string result="";
-        int i=0;
-        int n=s.length();
-
-        while(i<n)
-        {
-            char currentChar=s[i];
-            int count=0;
-
-            while(i<n && s[i]==currentChar)
-            {
-                count++;
-                i++;
-            }
-            result+=to_string(count)+currentChar;
-        }
-        return result;
+        string result = "1";
         
+        for (int i = 2; i <= n; i++) {
+            string temp = "";
+            int count = 1;
+            
+            for (int j = 0; j < result.length(); j++) {
+                if (j + 1 < result.length() && result[j] == result[j + 1]) {
+                    count++;
+                } else {
+                    temp += to_string(count) + result[j];
+                    count = 1;  
+                }
+            }
+            
+            result = temp;
+        }
+        
+        return result;
     }
 };
