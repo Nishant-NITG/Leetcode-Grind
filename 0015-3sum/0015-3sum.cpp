1c1d51233a215f8daa8976fc5a0a77f0ef86a708
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-09-24 22:01:23 +0530

    Time: 41 ms (87.35%), Space: 29 MB (92.41%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,59 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>result;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-
-        for(int i=0;i<n-2;i++)
-        {
-            if(i>0 and nums[i-1]==nums[i])
-            {
-                continue;
-            }
-
-            if(nums[i]>0)
-            {
-                break;
-
-            }
-
-            int left=i+1;
-            int right=n-1;
-            int target=-nums[i];
-
-            while(left<right)
-            {
-                int currsum=nums[left]+nums[right];
-
-                if(currsum==target)
-                {
-                    result.push_back({nums[i],nums[left],nums[right]});
-
-                    while(left<right && nums[left]==nums[left+1])
-                    {
-                        left++;
-                    }
-                    while(right>left && nums[right]==nums[right-1])
-                    {
-                        right--;
-                    }
-
-                     left++;
-                right--;
-                }
-
+        int n = nums.size();
+        vector<vector<int>> result;
+        
+        // Sort the array to enable two-pointer approach
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int target = -nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            
+            // Two Sum on the remaining array with target = -nums[i]
+            while (left < right) {
+                int sum = nums[left] + nums[right];
                 
-
-                else if (currsum < target) {
+                if (sum == target) {
+                    // Found a triplet
+                    result.push_back({nums[i], nums[left], nums[right]});
                     
+                    // Move past duplicates for both pointers
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < target) {
+                    // Need a larger sum, move left pointer right
                     left++;
                 } else {
-                    
+                    // Need a smaller sum, move right pointer left
                     right--;
                 }
             }
         }
+        
         return result;
     }
 };
