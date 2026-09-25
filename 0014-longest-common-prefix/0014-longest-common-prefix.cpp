339c68eaaa8867905f5836c8d4bc6e65e2e52e86
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-09-25 16:18:39 +0530

    Time: 0 ms (100%), Space: 11.7 MB (81.37%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,9 +3,12 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
         
+        // Compare each character of the first string against all others
         for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0][i];
+            
             for (int j = 1; j < strs.size(); j++) {
+                // If string j is too short or char doesn't match, we're done
                 if (i >= strs[j].length() || strs[j][i] != c) {
                     return strs[0].substr(0, i);
                 }
