6d5db33abb298db3213055ef57c4c75905187c62
Nishant Sharma <nishantsharmanit2023@gmail.com>
2026-09-26 18:43:41 +0530

    Time: 7 ms (45.93%), Space: 9.8 MB (63.86%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        string repeated="";
-        int count=0;
-
-        while(repeated.length()<b.length())
-        {
-            repeated+=a;
-            count++;
-        }
-
-        if(repeated.find(b)!=string::npos)
-        {
-            return count;
-        }
-
-        repeated+=a;
-        count++;
-
-        if(repeated.find(b)!=string::npos)
-        {
-            return count;
-        }
+        int lenA = a.length();
+        int lenB = b.length();
+        
+        // Minimum repetitions needed to cover length of b
+        int q = (lenB + lenA - 1) / lenA; // ceil division
+        
+        string repeated;
+        for (int i = 0; i < q; i++) repeated += a;
+        
+        if (repeated.find(b) != string::npos) return q;
+        
+        repeated += a;
+        if (repeated.find(b) != string::npos) return q + 1;
+        
         return -1;
     }
 };
